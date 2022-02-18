0bd509f56d646c868282d66b489d906ba3d959cb
OvercomingJunk <overcomingjunk@yandex.ru>
2022-02-18 21:03:19 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -17,3 +17,19 @@ public class Calculator {
         System.out.println(fourTimeTwo);
     }
 }
+
+ class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+}
