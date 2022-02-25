0729e8a48c6979828cd0f117d46d7093c20c689d
OvercomingJunk <overcomingjunk@yandex.ru>
2022-02-25 20:21:41 +0300

    4.3.6. Math.abs [#373616 #269173]

--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -134,8 +134,8 @@ public class ChessBoardTest {
         int y1 = 0;
         int x2 = 0;
         int y2 = 3;
-        int result = ChessBoard.way(x1, y1, x2, y2);
         int expected = 3;
+        int result = ChessBoard.way(x1, y1, x2, y2);
         Assert.assertEquals(expected, result);
     }
 }
