6295afe5e64ade92a9be08e4d378d4c9f974a24d
Artem Chernikov <overcomingjunk@yandex.ru>
2023-11-14 21:52:45 +0300

    TwoNumberSum

---
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class TwoNumberSum {
+    public static int[] getIndexes(int[] array, int target) {
+        int l = 0, r = array.length - 1;
+
+        while (l < r) {
+
+            int currentSum = array[l] + array[r];
+
+            if (currentSum == target) {
+                return new int[]{l, r};
+            } else if (currentSum > target) {
+                r--;
+            } else if (currentSum < target) {
+                l++;
+            }
+        }
+        return new int[0];
+    }
+}
---
+++ src/test/java/ru/job4j/array/TwoNumberSumTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TwoNumberSumTest {
+
+    @Test
+    public void whenTwoEqualsNumbersYesTarget() {
+        int[] array = {5, 5};
+        int target = 10;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {0, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenTwoEqualsNumbersNoTarget() {
+        int[] array = {5, 5};
+        int target = 12;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenWithNegativeNumbersYesTarget() {
+        int[] array = {-7, -5, 0, 5, 8, 12};
+        int target = 3;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {1, 4};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenWithoutNegativeNumbersYesTarget() {
+        int[] array = {0, 2, 5, 8, 10, 12};
+        int target = 15;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {2, 4};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+}
